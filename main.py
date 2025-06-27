d50955cb713c832dc7fc5501f5984064b90ee5c8
amanda <mndpasc@gmail.com>
2025-06-27 01:28:43 -0300

    te amo ruan

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 import os
-from os import getenv
+from apikeys import BOTTOKEN
 
 intents = nextcord.Intents.all()
 intents.message_content = True
@@ -21,4 +21,4 @@ async def on_ready():
     print("Todos os comandos foram sincronizados.")
     print(f"{bot.user.name} foi iniciado.")
 
-bot.run(getenv('MTM2OTU4ODY0OTgzNzk4NTg0NQ.GXupsP.tdsKklGmPnNEsNKqiHMD1Yjem7oA32_EJ8mDMQ'))
+bot.run(BOTTOKEN)
